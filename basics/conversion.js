524e1a28249f911faf2b4c4f2675046b5d9967c5
passionateee <shravyasrivastav2003@gmail.com>
2024-06-01 15:34:26 +0000

    conversion

--- basics/conversion.js
+++ basics/conversion.js
@@ -8,3 +8,17 @@ console.log(typeof valueInNumber);
 //"33" = 33
 //"33abc" = NaN[not a number]
 //true = 1; false = 0
+
+
+
+
+
+
+//=========================================================================================================================
+//1-  stack memory - primitive type use stack
+//2- heap memory - non primitive used the heap memory
+
+let myyoutubename  = "hiteshchaudharydotcom"
+let anothername = myyoutubename
+anothername = "chaiorcode"
+console.log(anothername);
