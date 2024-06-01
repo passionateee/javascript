838dd9f192fe939435513faf93c036d8178388a9
passionateee <shravyasrivastav2003@gmail.com>
2024-06-01 08:53:21 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser so this will give an error
+
+let name = "radhe krishna"
+let age = 67
+let isLoggedIn = false
+
+
+// PRIMITIVE DATATYPES IN JS//
+// number
+//boolean
+//string
+//bigint
+//null = representation of empty value // null is a object
+//undefined = humne value assign nhi ki
+//symbol = unique
+
+//object
+
+console.log(typeof "radhe krishna") // string
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 1552
+let accountEmail = "shravyasrivastav2003@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2//not allowed
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+prefer not to use 'var' bcz of issue in block scope and functional scope
+*/
